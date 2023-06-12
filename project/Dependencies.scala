dddd6ab946e1fafeeef7eefc304693155410ddd6
Scala Steward <scala_steward@virtuslab.com>
2023-06-12 14:46:56 +0000

    Update datafaker to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
     "com.softwaremill.retry" %% "retry" % "0.3.6",
     "org.bouncycastle" % "bcprov-jdk18on" % "1.73",
     "joda-time" % "joda-time" % "2.12.5",
-    "net.datafaker" % "datafaker" % "1.9.0",
+    "net.datafaker" % "datafaker" % "2.0.0",
     "com.github.mifmif" % "generex" % "1.0.2"
   )
 
