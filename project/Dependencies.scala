9733c16b71ab0212bfece6aed64788d8aa52e2c4
Scala Steward <scala_steward@virtuslab.com>
2023-11-15 16:10:53 +0000

    Update bcprov-jdk18on to 1.77

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     "org.apache.commons" % "commons-lang3" % "3.13.0",
     "javax.xml.bind" % "jaxb-api" % "2.3.1",
     "com.softwaremill.retry" %% "retry" % "0.3.6",
-    "org.bouncycastle" % "bcprov-jdk18on" % "1.76",
+    "org.bouncycastle" % "bcprov-jdk18on" % "1.77",
     "joda-time" % "joda-time" % "2.12.5",
     "net.datafaker" % "datafaker" % "2.0.2",
     "com.github.mifmif" % "generex" % "1.0.2"
